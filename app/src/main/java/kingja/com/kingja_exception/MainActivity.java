54afd158fe98fef63bb332acfc9d890c2f6d5b92
KingJA <kingjavip@gmail.com>
2016-06-15 17:29:24 +0800

    0615

--- app/src/main/java/kingja/com/kingja_exception/MainActivity.java
+++ app/src/main/java/kingja/com/kingja_exception/MainActivity.java
@@ -10,14 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AppManager.getAppManager().addActivity(this);
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-////                Thread.currentThread().setUncaughtExceptionHandler( CrashHandler.getInstance());
-//                Object o=null;
-//                o.toString();
-//            }
-//        }).start();
 
         Object o=null;
         o.toString();
